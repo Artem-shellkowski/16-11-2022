77c7fb23bca7f02a3b81acdb4568f659178e18ff
Artem-shellkowski <114939650+Artem-shellkowski@users.noreply.github.com>
2022-11-16 18:39:25 +0300

    Add files via upload

---
+++ 16-11-2022.cpp
@@ -0,0 +1,74 @@
+/*
+#include <iostream>
+#include <ctime>
+using namespace std;
+double powerA3(double a) {
+	return a * a * a;
+}
+int main() {
+	Proc1. Описать функцию powerA3(A), вычисляющую третью степень числа A и возвращающую ее (параметр вещественный). С помощью этой функции найти третьи степени пяти случайных чисел.
+	srand(time(nullptr));
+	for (int i = 1; i <= 5; i++) {
+	 double x = rand() % 11;
+	 double x3 = powerA3(x);
+	 cout << "x = " << x << " x^3 = " << x3 << endl;
+	}
+	*/
+/*
+#include <iostream>
+#include <ctime>
+using namespace std;
+double powerA3(double a) {
+	return a * a * a;
+}
+int main() {
+		srand(time(nullptr));
+	for (int i = 1; i <= 5; i++) {
+		double x = rand() % 11;
+		double x2 = powerA2(x);
+		double X4 = poverA4(x)
+		cout << "x = " << x << " x^2 = " << x2 <<"x^4 =" << X4 << endl;
+	}
+*/
+
+/*
+
+#include <iostream>
+#include <ctime>
+	using namespace std;
+	double aMean(double x, double y) {
+		return (x + y) / 2;
+
+	}
+	double gMean(double x, double y) {
+		return sqrt(x * y) / 2;
+
+	}
+	int main() {
+		srand(time(nullptr));
+		for (int i = 1; i <= 10; i++) {
+			double x1 = rand() % 101 * 0.1;
+			double y1 = rand() % 101 * 0.1;
+				cout << "x = " << x1 << " y = " << y1 << "sr_afirm" << aMean(x1, y1) << "sr_geom = " <<
+				gMean(x1, y1) << endl;
+		}
+	}
+*/
+#include <iostream>
+#include <ctime>
+using namespace std;
+double rectP(double x1, double y1, double x2, double y2) {
+}
+double rects(double x1, double y1, double x2, double y2) {
+	return
+}
+int main() {
+	setlocale(LC_ALL, "rus");
+	double a;
+	for (int i = 1; i <= 5; i++) {
+		cin >> x1 >> y1 >> x2 >> y2;
+		cout << "x = " << x1 <<"y1 = " << y1 << endl;
+	}
+
+	}
+
